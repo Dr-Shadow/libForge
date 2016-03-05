0defcfbd4f1f9c31448c74578c9e580bf651bb2a
Dr-Shadow <xxdrshadowxx@gmail.com>
2016-03-05 18:41:06 +0100

    Init repo libForge

---
+++ include/libForge.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef LIBFORGE_LIBFORGE_H
+#define LIBFORGE_LIBFORGE_H
+
+class libForge {
+public:
+  libForge() = default;
+  virtual ~libForge() = default;
+};
+
+#endif //LIBFORGE_LIBFORGE_H
---
+++ src/libForge.cpp
@@ -0,0 +1,1 @@
+#include "libForge.h"
